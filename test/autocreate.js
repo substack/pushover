7eae58373da0eb661f4513ecb8360345d42362c0
Richard Astbury <richard.astbury@gmail.com>
2016-05-06 21:37:55 +0100

    fixed bug with the `done` function not working on response, moved path.exists -> fs.exists

--- test/autocreate.js
+++ test/autocreate.js
@@ -12,22 +12,22 @@ var seq = require('seq');
 
 test('create, push to, and clone a repo', function (t) {
     t.plan(2);
-    
+
     var repoDir = '/tmp/' + Math.floor(Math.random() * (1<<30)).toString(16);
     var srcDir = '/tmp/' + Math.floor(Math.random() * (1<<30)).toString(16);
     var dstDir = '/tmp/' + Math.floor(Math.random() * (1<<30)).toString(16);
-    
+
     fs.mkdirSync(repoDir, 0700);
     fs.mkdirSync(srcDir, 0700);
     fs.mkdirSync(dstDir, 0700);
-    
+
     var repos = pushover(repoDir);
     var port = Math.floor(Math.random() * ((1<<16) - 1e4)) + 1e4;
     var server = http.createServer(function (req, res) {
         repos.handle(req, res);
     });
     server.listen(port);
-    
+
     process.chdir(srcDir);
     seq()
         .seq(function () {
@@ -57,7 +57,7 @@ test('create, push to, and clone a repo', function (t) {
                 .on('exit', this.ok)
         })
         .seq_(function (next) {
-            path.exists(dstDir + '/doom/a.txt', function (ex) {
+            exists(dstDir + '/doom/a.txt', function (ex) {
                 t.ok(ex, 'a.txt exists');
                 next();
             })
@@ -68,7 +68,7 @@ test('create, push to, and clone a repo', function (t) {
         })
         .catch(t.fail)
     ;
-    
+
     repos.on('push', function (push) {
         t.equal(push.repo, 'doom');
         push.accept();
